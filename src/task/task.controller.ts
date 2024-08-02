4a1f918abc941c9aca4624006d8cc1e54ba1b477
linuxJose <jcsilvafilho13@gmail.com>
2024-08-02 10:45:55 -0300

    Nest.Js Atualizacao 1.2

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { TaskDto } from './task.dto';
 import { TaskService } from './task.service';
 
@@ -9,5 +9,15 @@ export class TaskController {
   @Post()
   create(@Body() task: TaskDto) {
     this.taskService.create(task);
+  }
+
+  @Get('/:id')
+  finById(@Param('id') id: string): TaskDto {
+    return this.taskService.finById(id);
+  }
+
+  @Put()
+  update(@Body() task: TaskDto) {
+    this.taskService.update(task);
   }
 }
